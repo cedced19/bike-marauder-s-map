2856a506b156952ea065adbd6d154a56e5ef016d
Cédric JUNG <cedced19@gmail.com>
2017-12-27 09:41:40 +0100

    Create sync method

--- routes/coords-api.js
+++ routes/coords-api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var auth = require('../policies/auth');
 var fs = require('fs');
+var path = require('path');
 var coords;
 
 try {
@@ -15,10 +16,16 @@ router.get('/', auth, function(req, res) {
   res.json(coords);
 });
 
+/* GET Coords: get coords since a date */
+router.get('/sync/:date', auth, function(req, res) {
+  var arr = coords.filter(obj => obj.date > req.params.date);
+  res.json(arr);
+});
+
 /* POST Coords */
 router.post('/', function(req, res, next) {
   coords = coords.concat(req.body);
-  fs.writeFile('./coordinates.json', JSON.stringify(coords), function (err) {
+  fs.writeFile(path.resolve(__dirname, '../coordinates.json'), JSON.stringify(coords), function (err) {
     if (err) {
       err = new Error('Error saving the new coords.');
       err.status = 500;
